ae011aec4c04191fc1bbe16e035c13371ede675a
Unknown <dodekoekjes@gmail.com>
2018-05-16 16:06:32 +0200

    Init

---
+++ app/app/src/main/java/sh/surge/idp_groep12/idp_app/MainActivity.java
@@ -0,0 +1,13 @@
+package sh.surge.idp_groep12.idp_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
