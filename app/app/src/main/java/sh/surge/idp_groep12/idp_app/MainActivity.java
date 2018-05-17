8d79399162b7ed182a464b1c874a7cda394b1fc0
Unknown <dodekoekjes@gmail.com>
2018-05-17 19:45:48 +0200

    init

--- app/app/src/main/java/sh/surge/idp_groep12/idp_app/MainActivity.java
+++ app/app/src/main/java/sh/surge/idp_groep12/idp_app/MainActivity.java
@@ -2,6 +2,7 @@ package sh.surge.idp_groep12.idp_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        update();
+    }
+
+    public void update(){
+        TextView t = (TextView) findViewById(R.id.label);
+        t.setText("label test!");
+        TextView t2 = (TextView) findViewById(R.id.info);
+        t2.setText("info test");
     }
 }
